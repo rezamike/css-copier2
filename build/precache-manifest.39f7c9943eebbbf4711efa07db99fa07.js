7aaa323aaf30f17d825907ab4f2850f55c59ee0e
rezamike <michael.sanaiha@gmail.com>
2019-07-18 11:33:47 -0700

    init

---
+++ build/precache-manifest.39f7c9943eebbbf4711efa07db99fa07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8b1e0d69e70e06d8d8309cf7958bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00afcc8a227f22cd6141",
+    "url": "/static/css/app.css"
+  },
+  {
+    "revision": "f2dc7bcfb97721b5e1aa",
+    "url": "/static/css/content.css"
+  },
+  {
+    "revision": "8ea131521a8ba5a7c7b5",
+    "url": "/static/js/0.chunk.js"
+  },
+  {
+    "revision": "00afcc8a227f22cd6141",
+    "url": "/static/js/app.js"
+  },
+  {
+    "revision": "f2dc7bcfb97721b5e1aa",
+    "url": "/static/js/content.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
