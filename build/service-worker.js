2ff3efd571cd00da467161395f2bd9d7c0590528
rezamike <michael.sanaiha@gmail.com>
2019-07-22 14:18:26 -0700

    minor css changes for fun

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f04cef893e50d2ba56a13f90d930fe8.js"
+  "/precache-manifest.f6f150b732b9abddc87782178a33a059.js"
 );
 
 self.addEventListener('message', (event) => {
