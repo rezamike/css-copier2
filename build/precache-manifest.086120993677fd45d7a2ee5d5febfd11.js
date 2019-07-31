800a62083981c9ddfe7cabae9e59d27c49629c5c
Jamie Lo <jamielo@Jamies-MacBook-Pro.local>
2019-07-31 09:35:11 -0700

    ignore commit

---
+++ build/precache-manifest.086120993677fd45d7a2ee5d5febfd11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5746df05d102b1b075ea1f806413d0c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f182938e7bf29af0ee79",
+    "url": "/static/css/app.css"
+  },
+  {
+    "revision": "d22f2827bf489f602663",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "f182938e7bf29af0ee79",
+    "url": "/static/js/app.js"
+  },
+  {
+    "revision": "8453097b77c235d8209f",
+    "url": "/static/js/content.js"
+  }
+]);
